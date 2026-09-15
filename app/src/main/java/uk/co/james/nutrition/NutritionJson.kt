6e0e1af6dac62847e622a910d998c33445b31aec
James Traynor <Jtraynor1987@aol.com>
2026-09-15 14:48:41 +0100

    Fix Life Balance evidence gate compilation

--- app/src/main/java/uk/co/james/nutrition/NutritionJson.kt
+++
@@ -1,4 +0,0 @@
-package uk.co.james.nutrition
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-internal fun JsonObject.text(key:String):String=(get(key) as? JsonPrimitive)?.content.orEmpty()
--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
-}
