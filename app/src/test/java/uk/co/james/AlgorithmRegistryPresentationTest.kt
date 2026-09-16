db923fad2f7fb16beee07995d48d406a395814ce
James Traynor <Jtraynor1987@aol.com>
2026-09-16 13:33:00 +0100

    test: cover Life Balance v2 calibration semantics

--- app/src/test/java/uk/co/james/AlgorithmRegistryPresentationTest.kt
+++ app/src/test/java/uk/co/james/AlgorithmRegistryPresentationTest.kt
@@ -19,7 +19,7 @@ class AlgorithmRegistryPresentationTest {
         val entry=JamesAlgorithmRegistry.get("life_balance_v2")!!
         assertTrue(entry.inputs.contains("Time Ownership"))
         assertFalse(entry.inputs.any {it in setOf("RUT","Recovery","Low Mood","Activity")})
-        assertEquals(0.0,JamesCalibrationEngine.normalizeObserved("life_balance_v2","HARDLY ANY OF MY TIME FELT LIKE MINE"),0.0)
-        assertEquals(100.0,JamesCalibrationEngine.normalizeObserved("life_balance_v2","MY TIME LARGELY FELT LIKE MINE"),0.0)
+        assertEquals(0.0,JamesCalibrationEngine.normalizeObserved("life_balance_v2","HARDLY ANY OF MY TIME FELT LIKE MINE")!!,0.0)
+        assertEquals(100.0,JamesCalibrationEngine.normalizeObserved("life_balance_v2","MY TIME LARGELY FELT LIKE MINE")!!,0.0)
     }
 }
