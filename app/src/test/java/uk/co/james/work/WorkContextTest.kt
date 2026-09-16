07c4f8f02df04b7e990f1c220f161ea46d0742dc
James Traynor <Jtraynor1987@aol.com>
2026-09-16 19:39:43 +0100

    test: cover shift retraction migration

--- app/src/test/java/uk/co/james/work/WorkContextTest.kt
+++ app/src/test/java/uk/co/james/work/WorkContextTest.kt
@@ -15,6 +15,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import uk.co.james.data.JamesRepository
 import uk.co.james.database.JamesDatabase
+import uk.co.james.database.StoredRecord
 import uk.co.james.core.text
 
 @RunWith(RobolectricTestRunner::class)
